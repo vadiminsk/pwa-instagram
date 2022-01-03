14f5cdf492a9d86fb7a09a5011ec91901f169634
Vadim <vadiminsk90@gmail.com>
2022-01-03 21:38:03 +0300

    install prompt banner

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,15 +1,36 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
+
+  if (deferredPrompt) {
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(function (choiseResult) {
+      // Optionally, send analytics event with outcome of user choice
+      console.log(choiseResult.outcome);
+
+      if (choiseResult.outcome === "dismissed") {
+        console.log("User cancelled installation");
+      } else {
+        console.log("User added app to the homescreen");
+      }
+    });
+  }
+
+  // We've used the prompt, and can't use it again, throw it away
+  deferredPrompt = null;
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
